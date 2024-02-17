ed8cf022306a1573cd58bafc188cbb743e7f5ec5
andrew soulinhavong <soulinhavong@gmail.com>
2024-02-16 19:33:08 -0500

    complete

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+window.addEventListener('DOMContentLoaded', function() {
+    const paragraph = document.querySelector('p');
+    if (paragraph) {
+      paragraph.textContent = "This is really cool!";
+    }
+  });
